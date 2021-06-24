ce735219db88bcfd7bd14f6628950a089c828efa
Avinash M G <=>
2021-06-24 13:06:40 +0530

    video src changed to heroku

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,21 +4,20 @@ const VideoPlayer = () => {
   const [data, setData] = useState(false);
 
   useEffect(() => {
-    axios.get(`http://194.195.114.27:8080/`).then((res) => {
+    axios.get(`https://testsite-personal.herokuapp.com/`).then((res) => {
       setData(res.data);
       console.log(res.data);
     }, []);
   });
-  return (
-    <div id="video" className="videoPlayer">
-      <iframe
-        title="Youtube Video"
-        src={data}
-        frameborder="0"
-        allowfullscreen=""
-      ></iframe>
-    </div>
-  );
+  if (data !== false) {
+    return (
+      <div id="video" className="videoPlayer">
+        <iframe src={data} frameborder="0" allowfullscreen=""></iframe>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 };
 
 export default VideoPlayer;
